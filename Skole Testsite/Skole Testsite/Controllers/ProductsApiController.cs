b3af59be30b1608a757de73548088aad75ea9c95
Tjornfelt <madstjornfelt@gmail.com>
2013-10-17 13:01:16 +0200

    commit

---
+++ Skole Testsite/Skole Testsite/Controllers/ProductsApiController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Skole_Testsite.Models.HelperModels;
+using Umbraco.Web.WebApi;
+
+namespace Skole_Testsite.Controllers
+{
+    public class ProductsApiController : UmbracoApiController
+    {
+        Product[] products = new Product[] 
+        { 
+            new Product { Id = 1, Name = "Tomato Soup", Category = "Groceries", Price = 1 }, 
+            new Product { Id = 2, Name = "Yo-yo", Category = "Toys", Price = 3.75M }, 
+            new Product { Id = 3, Name = "Hammer", Category = "Hardware", Price = 16.99M } 
+        };
+
+        public IEnumerable<Product> GetAllProducts()
+        {
+            return products;
+        }
+
+        public Product GetProductById(int id)
+        {
+            var product = products.FirstOrDefault((p) => p.Id == id);
+            if (product == null)
+            {
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+            }
+            return product;
+        }
+    }
+}
